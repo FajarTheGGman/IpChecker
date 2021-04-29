b98473329449ab342daa5e332c5fa0558bdc134e
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2021-04-29 07:44:09 +0700

    Update ip.php

--- ip.php
+++ ip.php
@@ -14,19 +14,5 @@ foreach ($ex as $key) {
 $js = json_decode($key,true);
 print_r($js);
 
-#Dear Tukang Recode :
-#[=============Baca Hadist ini=============]
-#Lelaki yang mencuri dan wanita yang mencuri,potonglah tangan
-#keduanya (sebagai) pembalasan bagi apa yang mereka kerjakan dan
-#sebagai siksaan dari Allah.
-#Dan Allah Maha Perkasa lagi Maha Bijaksana.
-#Maka barangsiapa bertaubat (di antara pencuri-pencuri itu)
-#sesudah melakukan kejahatan itu dan memperbaiki diri,
-#maka sesungguhnya Allah menerima taubatnya.
-#Sesungguhnya Allah Maha Pengampun lagi Maha Penyayang.”
-
-#(QS. Al-Maidah: 38-39)
-  
-#Tinggal pakek doank apa susahnya sih 
 #Copyright© 2019 by Fajar Firdaus
 }
